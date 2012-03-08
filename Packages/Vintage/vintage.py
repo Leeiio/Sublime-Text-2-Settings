6c269c04a3c10acbc3efab2128bce23337bcf746
Leeiio <guaniu@gmail.com>
2012-03-08 18:54:01 +0800

    Update to Sublime Text 2 dev version 2183

--- Packages/Vintage/vintage.py
+++ Packages/Vintage/vintage.py
@@ -482,6 +482,14 @@ def shrink_inclusive(r):
 def shrink_exclusive(r):
     return sublime.Region(r.b, r.b, r.xpos())
 
+def shrink_to_first_char(r):
+    if r.b < r.a:
+        # If the Region is reversed, the first char is the character *before*
+        # the first bound.
+        return sublime.Region(r.a - 1)
+    else:
+        return sublime.Region(r.a)
+
 # This is the core: it takes a motion command, action command, and repeat
 # counts, and runs them all.
 #
@@ -611,7 +619,7 @@ class ViEval(sublime_plugin.TextCommand):
                 # Apply the action to the selection
                 self.view.run_command(action_command, action_args)
                 if reindent and self.view.settings().get('auto_indent'):
-                    self.view.run_command('reindent')
+                    self.view.run_command('reindent', {'force_indent': False})
 
         if not visual_mode:
             # Shrink the selection down to a point
@@ -743,7 +751,7 @@ class ViCopy(sublime_plugin.TextCommand):
     def run(self, edit, register = '"'):
         set_register(self.view, register, forward=True)
         set_register(self.view, '0', forward=True)
-        transform_selection_regions(self.view, lambda r: sublime.Region(r.a))
+        transform_selection_regions(self.view, shrink_to_first_char)
 
 class ViPasteRight(sublime_plugin.TextCommand):
     # Ensure the register is picked up from g_input_state, and that it'll be
@@ -920,7 +928,7 @@ class ReplaceCharacter(sublime_plugin.TextCommand):
             self.view.sel().add(s)
 
         if created_new_line and self.view.settings().get('auto_indent'):
-            self.view.run_command('reindent')
+            self.view.run_command('reindent', {'force_indent': False})
 
 class CenterOnCursor(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -939,12 +947,12 @@ class ScrollCursorLineToBottom(sublime_plugin.TextCommand):
 class ViIndent(sublime_plugin.TextCommand):
     def run(self, edit):
         self.view.run_command('indent')
-        transform_selection_regions(self.view, lambda r: sublime.Region(r.a))
+        transform_selection_regions(self.view, shrink_to_first_char)
 
 class ViUnindent(sublime_plugin.TextCommand):
     def run(self, edit):
         self.view.run_command('unindent')
-        transform_selection_regions(self.view, lambda r: sublime.Region(r.a))
+        transform_selection_regions(self.view, shrink_to_first_char)
 
 class ViSetBookmark(sublime_plugin.TextCommand):
     def run(self, edit, character):
